5e04b4ba68345928746b0665ccae2ab913139ec9
Argir Argirov <dj.giro@gmail.com>
2018-03-09 14:37:08 -0500

    using Yargs for command line arguments and passing functions from extern

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-console.log('hw');
 const fs = require('fs')
 const os = require('os')
 const _  = require('lodash')
+const yargs = require('yargs')
+
 const notes = require('./notes.js')
 
-let name = os.userInfo()
+// let name = os.userInfo()
 // fs.appendFileSync('grrret.txt', `hey ${name.username}`)
 const filteredArray = ['andrew',1,3,5,5,6,5,'aruiii', 'andrew']
 console.log(_.uniq(filteredArray));
+
+
+const argv = yargs.argv
+let command = argv._[0]
+
+console.log('process',process.argv);
+console.log('yargs',argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body)
+} else if (command === 'list') {
+  notes.getAll()
+} else if (command === 'read') {
+  notes.readNote(argv.title)
+} else if (command === 'remove') {
+  notes.removeNote(argv.title)
+} else {
+  console.log('command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,1 +1,21 @@
 console.log('starting notes');
+
+let addNote = (title, body) => {
+  console.log('adding note', title, body);
+}
+let getAll = () => {
+  console.log('listing notes');
+}
+let readNote = (title) => {
+  console.log('reading note', title);
+}
+let removeNote = (title) => {
+  console.log('removing note', title);
+}
+
+module.exports = {   //makes the bellow a function you can call on notes
+  addNote: addNote,
+  getAll,
+  readNote,
+  removeNote
+}
